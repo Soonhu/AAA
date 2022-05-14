1a3d8bcaae6efb58dbbe260d092ca5438f0ff3c3
Soonhu <98966042+Soonhu@users.noreply.github.com>
2022-05-14 20:37:59 +0800

    Update sub_info.js

--- Scripts/sub_info.js
+++ Scripts/sub_info.js
@@ -38,10 +38,10 @@ let args = getArgs();
   let used = info.download + info.upload;
   let total = info.total;
   let expire = args.expire || info.expire;
-  let content = [`用量：${bytesToSize(used)}  |  ${bytesToSize(total)}`];
+  let content = [`用量：${bytesToSize(used)} | ${bytesToSize(total)}`];
 
   if (resetDayLeft) {
-    content.push(`剩余：${bytesToSize(total-used)}  | 重置:${resetDayLeft}天`);
+    content.push(`剩余：${bytesToSize(total-used)} |重置:${resetDayLeft}天`);
   }
   if (expire && expire !== "false") {
     if (/^[\d.]+$/.test(expire)) expire *= 1000;
