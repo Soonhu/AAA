76fcfc79c7a32545524d71d7defbe7aaedadddda
Soonhu <98966042+Soonhu@users.noreply.github.com>
2022-05-14 20:15:54 +0800

    Update timecard.js

--- Scripts/timecard.js
+++ Scripts/timecard.js
@@ -90,7 +90,7 @@ function icon_now(num){
 $done({
 title:title_random(tnumcount(Number(nowlist))),
 icon:icon_now(tnumcount(Number(nowlist))),
-"icon-color":"#E94335",
+"icon-color":"#FF2D55",
 content:tlist[nowlist][0]+":"+today(tnumcount(nowlist))+"天,"+tlist[Number(nowlist) + Number(1)][0] +":"+ tnumcount(Number(nowlist) + Number(1))+ "天,"+tlist[Number(nowlist) + Number(2)][0]+":"+tnumcount(Number(nowlist) + Number(2))+"天"
 })
 
