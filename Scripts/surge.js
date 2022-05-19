267a324eceaf56453e75770dc8ca13b144f7266e
Soonhu <98966042+Soonhu@users.noreply.github.com>
2022-05-19 10:23:06 +0800

    Update surge.js

--- Scripts/surge.js
+++ Scripts/surge.js
@@ -19,7 +19,7 @@ $done({
     title:"Surge  已运行"+startTime,
     content:"Mitm:"+icon_status(mitm_status.enabled)+"  Rewrite:"+icon_status(rewrite_status.enabled)+"  Scripting:"+icon_status(scripting_status.enabled),
     icon: icon_s?"power.circle":"exclamationmark.triangle",
-   "icon-color":icon_s?"#DC143C":"#FF7500"
+   "icon-color":icon_s?"#FF2D55":"#FF7500"
 });
 })();
 function icon_status(status){
