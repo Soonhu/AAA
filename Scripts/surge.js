c4f5192a6eac170e21ed39a0b7837bfb99665360
Soonhu <98966042+Soonhu@users.noreply.github.com>
2022-06-23 14:03:35 +0800

    Update surge.js

--- Scripts/surge.js
+++ Scripts/surge.js
@@ -16,8 +16,7 @@ if ($trigger == "button") {
 	$notification.post("配置重载","配置重载成功","")
 };
 $done({
-    title:"Surge  已运行"+startTime ,
-    content:"-------------------------------"('\n')+"Mitm:"+icon_status(mitm_status.enabled)+"  Rewrite:"+icon_status(rewrite_status.enabled)+"  Scripting:"+icon_status(scripting_status.enabled),
+    title:"Surge  已运行"+startTime,      content:"--------------------------"+"Mitm:"+icon_status(mitm_status.enabled)+"  Rewrite:"+icon_status(rewrite_status.enabled)+"  Scripting:"+icon_status(scripting_status.enabled),
     icon: icon_s?"power.circle":"exclamationmark.triangle",
    "icon-color":icon_s?"#FF2D55":"#FF7500"
 });
